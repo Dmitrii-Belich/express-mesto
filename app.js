1fb8903dabcfacfd38ffe31ede275b4d2c136650
Dmitrii Belich <belich.ds@yandex.ru>
2020-08-14 14:01:41 +0300

    feat: npm scripts

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const cards = require('./routes/cards');
 const users = require('./routes/users');
 
 const { PORT = 3000 } = process.env;
-
 const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/cards', cards);
